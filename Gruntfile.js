c0da70a5dfedc87ca181656eefde15b43f6ea5fe
Fengfeike <2088399108@qq.com>
2020-03-18 17:43:46 +0800

    111

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,21 @@
 module.exports = function (grunt) {
-  grunt.initConfig({
-    uglify: {
-      './dist/index.js':'./index.js'       
-  }
-               
-  });
+    grunt.initConfig({
+      htmlmin: {
+        options: {
+          removeComments:true,  
+          cllapseWhitespace: true
+        },
+    files: {
+            src: './index.html',
+            dest: 'dist/index.html'        
+    }
+        
+      }
+                 
+    });
 
-      grunt.loadNpmTasks('grunt-contrib-uglify');
+      grunt.loadNpmTasks('grunt-contrib-htmlmin');
+      grunt.registerTask('default', ['htmlmin']); 
 
-      grunt.registerTask('default', ['uglify']); 
+  };
 
-};
